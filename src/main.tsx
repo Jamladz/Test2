ce5042593285b843ec4817ad309d20006beb9a38
Jamladz <piswap014@gmail.com>
2026-04-10 20:35:01 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,15 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import App from './App.tsx';
+import './index.css';
 
+const manifestUrl = "https://raw.githubusercontent.com/Jamladz/Test2/main/tonconnect-manifest.json";
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>
+  </StrictMode>,
+);
