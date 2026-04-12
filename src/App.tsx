bf88b90c2f00d6c62627c1ca8d9b0eaf3eb66830
Jamladz <piswap014@gmail.com>
2026-04-12 22:18:30 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1286,7 +1286,7 @@ const TasksTab: React.FC<{ t: any, balance: number, setBalance: React.Dispatch<R
     if (now < adsNextTime || adsWatched >= 30) return;
     playSound('click');
     if ((window as any).Adsgram) {
-      const AdController = (window as any).Adsgram.init({ blockId: "int-27580" });
+      const AdController = (window as any).Adsgram.init({ blockId: "int-27598" });
       AdController.show().then(() => {
         const newWatched = adsWatched + 1;
         setAdsWatched(newWatched);
